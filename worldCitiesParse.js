4ef67184873ce9461ea5788d51e7750261261903
Kevin Sivabalan <jbosshellokitty@gmail.com>
2014-11-16 21:10:23 -0500

    changes

--- worldCitiesParse.js
+++ worldCitiesParse.js
@@ -2,14 +2,31 @@ var fs = require('fs');
 var textFile = process.argv[2];
 var LineByLineReader = require('line-by-line');
 var outputFilename = 'my.json';
+var outputFilenameDone = 'done.json'
+var geoJSON = 'my.geojson';
+var geoJSONDone = 'done.geojson';
 var status = require('node-status');
 var console = status.console();
 var stream = fs.createWriteStream(outputFilename);
+var geoStream = fs.createWriteStream(geoJSON);
 
 if(!textFile) return console.log('Done');
 
 var arrayOfCities = [];
+var arrayOfGeoCities = [];
+
 var lr = new LineByLineReader(textFile);
+var count = 0;
+
+var counter = function () {
+  setTimeout(function () {
+    ++count;
+    process.stdout.clearLine();  // clear current text
+    process.stdout.cursorTo(0);  // move cursor to beginning of line}
+    process.stdout.write("Lines " + count)
+  },1);
+}
+
 
 console.log("Start");
 
@@ -42,6 +59,7 @@ lr.on('line', function (line) {
   // pause emitting of lines...
   lr.pause();
   totalLineCount.inc();
+  counter();
   // ...do your asynchronous line processing..
   setTimeout(function () {
     var _line = line.split(',');
@@ -55,15 +73,33 @@ lr.on('line', function (line) {
       Longitude: _line[6]
     }
     totalLineCount.inc();
-    if(city.Population < 1) {
-      skippedLines.inc();
-      return lr.resume();
+    // if(city.Population < 1) {
+    //   skippedLines.inc();
+    //   return lr.resume();
+    // }
+    var geoCity = {
+      type: "Feature",
+      geometry:{
+        type: "Point",
+        coordinates: [city.Longitude,city.Latitude],
+        properties:{
+          Country: _line[0],
+          City: _line[1],
+          AccentCity: _line[2],
+          Region: _line[3],
+          Population: _line[4],
+        }
+      }
     }
+
     parsedLines.inc();
-    console.log(city);
-    arrayOfCities.push(city);  
+    // console.log(city);
     var cityString = JSON.stringify(city);
-    stream.write(cityString + '\n')
+    var geoCityString = JSON.stringify(geoCity);    
+    arrayOfCities.push(city);  
+    arrayOfGeoCities.push(geoCityString);
+    stream.write(cityString + '\n');
+    geoStream.write(geoCityString + '\n');
     // ...and continue emitting lines.
     lr.resume();
   }, 0);
@@ -71,13 +107,21 @@ lr.on('line', function (line) {
 
 lr.on('end', function () {
   // All lines are read, file is closed now.
-  // fs.writeFile(outputFilename, JSON.stringify(arrayOfCities, null, 4), function(err) {
-  //   if(err) {
-  //     console.log(err);
-  //   } else {
-  //     console.log("JSON saved to " + outputFilename);
-  //   }
-  // });
+  fs.writeFile(outputFilenameDone, JSON.stringify(arrayOfCities, null, 4), function(err) {
+    if(err) {
+      console.log(err);
+    } else {
+      console.log("JSON saved to " + outputFilenameDone);
+    }
+  });
+  fs.writeFile(geoJSONDone, JSON.stringify(arrayOfGeoCities, null, 4), function(err) {
+    if(err) {
+      console.log(err);
+    } else {
+      console.log("JSON saved to " + outputFilenameDone);
+    }
+  });
+  geoStream.close();
   stream.close();
   status.stop();
   console.log('Done');
